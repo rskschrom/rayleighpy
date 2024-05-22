7a9902d2e70ef3b416e64f1fb17d206a024e49c7
me <rschrom.science@gmail.com>
2024-05-22 09:19:52 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='rayleighpy',
+      version='0.0.1',
+      packages=['rayleighpy'],
+      package_dir={'rayleighpy':'src'})
---
+++ src/polarizability.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+# polarizability for ellipsoid
+def ellipsoid(a, b, c, eps):
+
